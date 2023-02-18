23ae34b1f60ba6b6d3b0f77077b785964d087801
Alfredo Polanco <alfredpolanco@Alfredos-MacBook-Pro.local>
2023-02-18 14:25:26 -0400

    Done with Dashabord Test Cases

--- tests/dashboardTests.spec.js
+++ tests/dashboardTests.spec.js
@@ -9,14 +9,56 @@ test.describe('Dashboard tests', () => {
 		await login.login();
 	});
 
+	test.afterEach(async ({ page }) => {
+		const dashboard = new DashboardPage(page);
+		await dashboard.logout();
+	});
+
+	test('Check Time at Work section', async ({ page }) => {
+		const dashboard = new DashboardPage(page);
+		await dashboard.timeAtWork();
+		await expect(dashboard.timeAtWorkTitle).toHaveText([`Time at Work`]);
+	});
+
 	test('Check My Actions section', async ({ page }) => {
 		const dashboard = new DashboardPage(page);
 		await dashboard.myActions();
 		await expect(dashboard.myActionsTitle).toHaveText([`My Actions`]);
 	});
 
-	test.afterAll(async ({ page }) => {
+	test('Check Quick Launch section', async ({ page }) => {
 		const dashboard = new DashboardPage(page);
-		await dashboard.logout();
+		await dashboard.quickLaunch();
+		await expect(dashboard.quickLaunchTitle).toHaveText([`Quick Launch`]);
+	});
+
+	test('Check Buzz Latest Posts', async ({ page }) => {
+		const dashboard = new DashboardPage(page);
+		await dashboard.buzzLatestPost();
+		await expect(dashboard.BuzzLatestPostTitle).toHaveText([`Buzz Latest Posts`]);
+	});
+
+	test('Check Employees on Leave', async ({ page }) => {
+		const dashboard = new DashboardPage(page);
+		await dashboard.employeeOnLeaveToday();
+		await expect(dashboard.employeeOnLeaveTodayTittle).toHaveText([
+			`Employees on Leave Today`,
+		]);
+	});
+
+	test('Check Employee Distribution by Sub Unit', async ({ page }) => {
+		const dashboard = new DashboardPage(page);
+		await dashboard.employeeDistributionBySubUnit();
+		await expect(dashboard.employeeDistributionBySubUnitTitle).toHaveText([
+			`Employee Distribution by Sub Unit`,
+		]);
+	});
+
+	test('Check Employee Distribution by Location', async ({ page }) => {
+		const dashboard = new DashboardPage(page);
+		await dashboard.employeeDistributionByLocation();
+		await expect(dashboard.employeeDistributionByLocationTitle).toHaveText([
+			`Employee Distribution by Location`,
+		]);
 	});
 });
